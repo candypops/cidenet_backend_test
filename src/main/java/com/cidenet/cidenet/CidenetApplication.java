075d7930c485023cd3f82c11f83f7fef10bac157
Maria Gabriela <mariagabriela236@gmail.com>
2022-05-13 19:36:10 -0400

    initial commit

---
+++ src/main/java/com/cidenet/cidenet/CidenetApplication.java
@@ -0,0 +1,13 @@
+package com.cidenet.cidenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidenetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cidenet/cidenet/CidenetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cidenet.cidenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidenetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
