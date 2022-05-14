07b1abaefa3bde315b4ffc24776c37cde7428b14
Maria Gabriela <mariagabriela236@gmail.com>
2022-05-14 19:46:37 -0400

    traer tipo de identificacion activos en respository

--- src/main/java/com/cidenet/cidenet/repository/IdentificationTypeRepository.java
+++ src/main/java/com/cidenet/cidenet/repository/IdentificationTypeRepository.java
@@ -2,6 +2,13 @@ package com.cidenet.cidenet.repository;
 
 import com.cidenet.cidenet.model.IdentificationType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface IdentificationTypeRepository extends JpaRepository<IdentificationType, Long> {
+
+    @Query("SELECT i FROM IdentityType i WHERE i.status = 'ACTIVE'")
+    List<IdentificationType> findAllActiveIdentificationTypes();
+
 }
