063a6396fd341831b200251457b0865a6615a2a5
Maria Gabriela <mariagabriela236@gmail.com>
2022-05-14 01:01:16 -0400

    servicio para area de trabajo

---
+++ src/main/java/com/cidenet/cidenet/service/WorkFieldService.java
@@ -0,0 +1,23 @@
+package com.cidenet.cidenet.service;
+
+import com.cidenet.cidenet.model.WorkField;
+import com.cidenet.cidenet.repository.WorkFieldRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WorkFieldService {
+
+    private final WorkFieldRepository workFieldRepository;
+
+    @Autowired
+    public WorkFieldService(WorkFieldRepository workFieldRepository) {
+        this.workFieldRepository = workFieldRepository;
+    }
+
+    public List<WorkField> getWorkFields(){
+        return workFieldRepository.findAll();
+    }
+}
