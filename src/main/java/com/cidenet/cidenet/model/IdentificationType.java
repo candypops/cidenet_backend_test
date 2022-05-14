2ab6b51e54e2b5173f5fa3eedb93125592b9656d
Maria Gabriela <mariagabriela236@gmail.com>
2022-05-14 19:28:52 -0400

    getters, setters y constructor

--- src/main/java/com/cidenet/cidenet/model/IdentificationType.java
+++ src/main/java/com/cidenet/cidenet/model/IdentificationType.java
@@ -13,5 +13,37 @@ public class IdentificationType {
     private String name;
 
     @Column(nullable = false)
-    private boolean status;
+    private String status;
+
+    public IdentificationType() {
+    }
+
+    public IdentificationType(String name, String status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
