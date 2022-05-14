b26644a9dccadd2f7a13540c4d1e5697107826a9
Maria Gabriela <mariagabriela236@gmail.com>
2022-05-14 19:20:43 -0400

    devolviendo areas de trabajo activas

--- src/main/java/com/cidenet/cidenet/repository/WorkFieldRepository.java
+++ src/main/java/com/cidenet/cidenet/repository/WorkFieldRepository.java
@@ -2,6 +2,12 @@ package com.cidenet.cidenet.repository;
 
 import com.cidenet.cidenet.model.WorkField;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface WorkFieldRepository extends JpaRepository<WorkField, Long> {
+
+    @Query("SELECT w FROM WorkField w WHERE w.status = 'ACTIVE'")
+    List<WorkField> findAllActiveWorkfields();
 }
