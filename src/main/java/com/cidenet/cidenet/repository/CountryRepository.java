6b4f24a611547cde81c48d56921879ad00ebdf88
Maria Gabriela <mariagabriela236@gmail.com>
2022-05-14 19:45:36 -0400

    creacion de repositiory para pais con funcion para traer paises activos

---
+++ src/main/java/com/cidenet/cidenet/repository/CountryRepository.java
@@ -0,0 +1,13 @@
+package com.cidenet.cidenet.repository;
+
+import com.cidenet.cidenet.model.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
+    @Query("SELECT c FROM Country c WHERE c.status = 'ACTIVE'")
+    List<Country> findAllActiveCountries();
+}
