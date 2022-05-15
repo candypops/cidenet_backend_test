891616ddcf104e72b9055f352c083a6271e6e655
Maria Gabriela <mariagabriela236@gmail.com>
2022-05-15 16:35:28 -0400

    agregado manejos de errores de sql, mal formato en campo y error al mapear json

--- src/main/java/com/cidenet/cidenet/exception/RestExceptionHandler.java
+++ src/main/java/com/cidenet/cidenet/exception/RestExceptionHandler.java
@@ -1,26 +1,57 @@
 package com.cidenet.cidenet.exception;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.sql.SQLException;
+import java.util.Optional;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
-public class RestExceptionHandler extends ResponseEntityExceptionHandler {
-    @Override
+public class RestExceptionHandler {
+
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         String error = "JSON mal formateado";
-        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex));
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error));
     }
-
 
     private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
         return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleException(MethodArgumentNotValidException ex) {
+        System.out.println(ex.getBindingResult().getAllErrors().stream().findFirst());
+        Optional<ObjectError> firstError = ex.getBindingResult().getAllErrors().stream().findFirst();
+        if (firstError.isPresent()) {
+            final String firstErrorMessage = firstError.get().getDefaultMessage();
+            return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, firstErrorMessage));
+        }
+        String error = "MethodArgumentNotValidException";
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error));
+
+    }
+
+    @ExceptionHandler(JsonMappingException.class)
+    public ResponseEntity<Object> handleJsonMappingException(JsonMappingException ex) {
+        String error = "JsonMappingException";
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex));
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public  ResponseEntity<Object> handleSQLException( SQLException ex){
+        String error = "SQLException";
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex));
+
     }
 }
