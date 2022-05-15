06b0fcf498e0b8b624077cac4501376860904c2d
Maria Gabriela <mariagabriela236@gmail.com>
2022-05-15 01:40:42 -0400

    listar todos los empleados y crear nuevo empleado

--- src/main/java/com/cidenet/cidenet/rest/EmployeeREST.java
+++ src/main/java/com/cidenet/cidenet/rest/EmployeeREST.java
@@ -1,17 +1,21 @@
 package com.cidenet.cidenet.rest;
 
 import com.cidenet.cidenet.model.Country;
+import com.cidenet.cidenet.model.Employee;
 import com.cidenet.cidenet.model.IdentificationType;
 import com.cidenet.cidenet.model.WorkField;
 import com.cidenet.cidenet.service.CountryService;
+import com.cidenet.cidenet.service.EmployeeService;
 import com.cidenet.cidenet.service.IdentificationTypeService;
 import com.cidenet.cidenet.service.WorkFieldService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotEmpty;
+import java.net.URI;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -21,15 +25,17 @@ public class EmployeeREST {
     private final WorkFieldService workFieldService;
     private final CountryService countryService;
     private final IdentificationTypeService identificationTypeService;
+    private final EmployeeService employeeService;
 
     @Autowired
     public EmployeeREST(WorkFieldService workFieldService,
                         CountryService countryService,
-                        IdentificationTypeService identificationTypeService
-                        ) {
+                        IdentificationTypeService identificationTypeService,
+                        EmployeeService employeeService) {
         this.workFieldService = workFieldService;
         this.countryService = countryService;
         this.identificationTypeService = identificationTypeService;
+        this.employeeService = employeeService;
     }
 
     @GetMapping
@@ -49,4 +55,24 @@ public class EmployeeREST {
     private  ResponseEntity<List<IdentificationType>> getAllActiveIdentificationTypes(){
         return  ResponseEntity.ok(identificationTypeService.getAllActiveIdentificationTypes());
     }
+
+    @PostMapping
+    @RequestMapping(path = "/save-employee")
+    private ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
+        employeeService.something();
+        try {
+            employeeService.saveNewEmployee(employee);
+            return ResponseEntity.created(new URI("api/v1/save-employee"+employee.getEmail())).body(employee);
+        }catch (Exception e){
+
+            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+    
+    @GetMapping
+    @RequestMapping(path = "/employees")
+    private ResponseEntity<List<Employee>> getAllEmployees(){
+        return  ResponseEntity.ok(employeeService.getAllActiveEmployees());
+    }
+
 }
