1fcb89ca5b0b0eb97ccc7db90b8b94c6994020f3
Maria Gabriela <mariagabriela236@gmail.com>
2022-05-15 01:41:29 -0400

    select todos los empleados en repository

--- src/main/java/com/cidenet/cidenet/repository/EmployeeRepository.java
+++ src/main/java/com/cidenet/cidenet/repository/EmployeeRepository.java
@@ -2,6 +2,13 @@ package com.cidenet.cidenet.repository;
 
 import com.cidenet.cidenet.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query("SELECT e FROM Employee e WHERE e.status='ACTIVE'")
+    List<Employee> getAllActiveEmployees();
+
 }
