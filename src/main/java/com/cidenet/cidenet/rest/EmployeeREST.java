5697edb5b48adf3c196496866d0ea0b7a93db17a
Maria Gabriela <mariagabriela236@gmail.com>
2022-05-16 16:29:51 -0400

    funcion editar empleado y agregadas fechas de creacion y de modificacion

--- src/main/java/com/cidenet/cidenet/rest/EmployeeREST.java
+++ src/main/java/com/cidenet/cidenet/rest/EmployeeREST.java
@@ -1,6 +1,9 @@
 package com.cidenet.cidenet.rest;
 
 import com.cidenet.cidenet.dto.EmployeeRequest;
+import com.cidenet.cidenet.exception.DuplicateIdentificationException;
+import com.cidenet.cidenet.exception.ResourceNotFoundException;
+import com.cidenet.cidenet.exception.UnexpectedErrorException;
 import com.cidenet.cidenet.model.Country;
 import com.cidenet.cidenet.model.Employee;
 import com.cidenet.cidenet.model.IdentificationType;
@@ -18,8 +21,11 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
@@ -50,58 +56,91 @@ public class EmployeeREST {
 
     @GetMapping
     @RequestMapping(path = "/get-countries")
-    private ResponseEntity<List<Country>> getAllActiveCountries(){
+    private ResponseEntity<List<Country>> getAllActiveCountries() {
         return ResponseEntity.ok(countryService.getAllActiveCountries());
     }
 
     @GetMapping
     @RequestMapping(path = "/get-identification-types")
-    private  ResponseEntity<List<IdentificationType>> getAllActiveIdentificationTypes(){
-        return  ResponseEntity.ok(identificationTypeService.getAllActiveIdentificationTypes());
+    private ResponseEntity<List<IdentificationType>> getAllActiveIdentificationTypes() {
+        return ResponseEntity.ok(identificationTypeService.getAllActiveIdentificationTypes());
     }
 
     @PostMapping
-    @RequestMapping(path = "/save-employee")
-    private ResponseEntity<Employee> saveEmployee(@RequestBody @Valid EmployeeRequest employee){
+    @RequestMapping(path = "/save-employee", method = RequestMethod.POST)
+    private ResponseEntity<Employee> saveEmployee(@RequestBody @Valid EmployeeRequest employee) {
+        Employee e = buildEmployee(employee);
+        e.setCreationDate(getCurrentDate());
+        e.setModificationDate(getCurrentDate());
+        e = employeeService.saveNewEmployee(e);
+        try {
+            return ResponseEntity.created(new URI("api/v1/save-employee" + e.getEmail())).body(e);
+        } catch (UnexpectedErrorException ex){
+            throw  new UnexpectedErrorException();
+        }catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @GetMapping
+    @RequestMapping(path = "/employees")
+    private ResponseEntity<List<Employee>> getAllEmployees() {
+        return ResponseEntity.ok(employeeService.getAllActiveEmployees());
+    }
+
+    @GetMapping
+    @RequestMapping(path = "/employees/paged")
+    private ResponseEntity<Page<Employee>> getAllEmployees(Pageable pageable) {
+        return ResponseEntity.ok(employeeService.getAllActiveEmployees(pageable));
+    }
+
+    @PutMapping
+    @RequestMapping(path = "/update-employee", method = RequestMethod.PUT)
+    private ResponseEntity<Employee> updateEmployee(@RequestBody @Valid EmployeeRequest employee) {
+        Employee e = buildEmployee(employee);
+        e.setId((long) employee.getId());
+        e.setModificationDate(getCurrentDate());
+        try {
+            e = employeeService.editEmployee(e);
+        } catch (ResourceNotFoundException ex) {
+            throw new ResourceNotFoundException();
+        } catch (DuplicateIdentificationException ex) {
+            throw new DuplicateIdentificationException();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return ResponseEntity.ok(e);
+    }
+
+    private String getCurrentDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+        return sdf.format(new Date());
+    }
+
+    private Employee buildEmployee(EmployeeRequest employee) throws UnexpectedErrorException {
         Optional<IdentificationType> optionalIdentificationType =
                 identificationTypeService.getIdentificationTypeById((long) employee.getIdentificationTypeCode());
         Optional<Country> optionalCountry =
                 countryService.getCountryById((long) employee.getCountryCode());
         Optional<WorkField> optionalWorkField =
                 workFieldService.getWorkFieldById((long) employee.getWorkFieldCode());
-        Employee e = new Employee( "email", "ACTIVE",
+        String status = Objects.isNull(employee.getStatus()) ? "ACTIVE" : employee.getStatus().toString();
+        Employee e = new Employee("email", status,
                 employee.getFirstName(), employee.getMiddleName(),
                 employee.getLastName(), employee.getSecondLastName(),
-                employee.getIdentificationNumber(), new Date(), new Date());
-        if(optionalIdentificationType.isPresent() && optionalCountry.isPresent() && optionalWorkField.isPresent()){
+                employee.getIdentificationNumber());
+        if (optionalIdentificationType.isPresent() && optionalCountry.isPresent() && optionalWorkField.isPresent()) {
             IdentificationType identificationType = optionalIdentificationType.get();
             Country country = optionalCountry.get();
             WorkField workField = optionalWorkField.get();
             e.setCountry(country);
             e.setIdentificationType(identificationType);
             e.setWorkField(workField);
-
+        }else{
+            throw new UnexpectedErrorException();
         }
-        employeeService.saveNewEmployee(e);
-        try {
-            return ResponseEntity.created(new URI("api/v1/save-employee"+e.getEmail())).body(e);
-        }catch (Exception ex){
-            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+        return e;
     }
-
-    @GetMapping
-    @RequestMapping(path = "/employees")
-    private ResponseEntity<List<Employee>> getAllEmployees(){
-        return  ResponseEntity.ok(employeeService.getAllActiveEmployees());
-    }
-
-    @GetMapping
-    @RequestMapping(path = "/employees/paged")
-    private ResponseEntity<Page<Employee>> getAllEmployees(Pageable pageable){
-        return  ResponseEntity.ok(employeeService.getAllActiveEmployees(pageable));
-    }
-
 
 
 }
