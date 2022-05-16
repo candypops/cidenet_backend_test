60e66218a5823de076c08ffd01270d39e3cfb207
Maria Gabriela <mariagabriela236@gmail.com>
2022-05-16 16:20:54 -0400

    agregada notacion a repositorio

--- src/main/java/com/cidenet/cidenet/repository/IdentificationTypeRepository.java
+++ src/main/java/com/cidenet/cidenet/repository/IdentificationTypeRepository.java
@@ -3,9 +3,11 @@ package com.cidenet.cidenet.repository;
 import com.cidenet.cidenet.model.IdentificationType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface IdentificationTypeRepository extends JpaRepository<IdentificationType, Long> {
 
     @Query("SELECT i FROM IdentificationType i WHERE i.status = 'ACTIVE'")
