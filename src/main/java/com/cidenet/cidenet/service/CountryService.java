1ac3317e826de7653813b67aea6f864b6458cb06
Maria Gabriela <mariagabriela236@gmail.com>
2022-05-15 22:51:35 -0400

    buscar pais por id

--- src/main/java/com/cidenet/cidenet/service/CountryService.java
+++ src/main/java/com/cidenet/cidenet/service/CountryService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CountryService {
@@ -18,5 +19,10 @@ public class CountryService {
 
     public List<Country> getAllActiveCountries() {
         return countryRepository.findAllActiveCountries();
+    }
+    
+    public Optional<Country> getCountryById(Long id){
+        return countryRepository.findById(id);
+        
     }
 }
