e270d90f8b9fa3aa22284f40497e3c3e7c6feb91
Maria Gabriela <mariagabriela236@gmail.com>
2022-05-15 20:22:04 -0400

    funcion generar email

--- src/main/java/com/cidenet/cidenet/service/EmployeeService.java
+++ src/main/java/com/cidenet/cidenet/service/EmployeeService.java
@@ -4,6 +4,7 @@ import com.cidenet.cidenet.model.Employee;
 import com.cidenet.cidenet.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -17,24 +18,36 @@ public class EmployeeService {
         this.employeeRepository = employeeRepository;
     }
 
-    public List<Employee> getAllActiveEmployees(){
+    public List<Employee> getAllActiveEmployees() {
         return employeeRepository.getAllActiveEmployees();
     }
 
-    public void saveNewEmployee(@Valid Employee employee){
+    public void saveNewEmployee(@Valid Employee employee) {
+        generateEmail(employee, 0);
         employeeRepository.save(employee);
     }
 
-    public boolean isEmailDuplicated(String email){
+    public boolean isEmailDuplicated(String email) {
         return employeeRepository.findByEmail(email) != null;
     }
 
-    public Employee editEmployee(Employee employee){
+    public Employee editEmployee(Employee employee) {
         return employee;
     }
 
-    public void something(){
+    public void something() {
         System.out.println("AAAAAAAAAAAAAAAAAAAA");
+    }
+
+    public void generateEmail(Employee employee, int id) {
+        String identifier = id > 0 ? "." + id : "";
+        String email = StringUtils.trimAllWhitespace(employee.getLastName()) + "." + employee.getFirstName() +
+                identifier + "@cidenet.com";
+        if (isEmailDuplicated(email)) {
+            generateEmail(employee, id+1);
+        }else{
+            employee.setEmail(email);
+        }
     }
 
 
