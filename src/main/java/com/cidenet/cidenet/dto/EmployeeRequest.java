4f46b2653e9aad2b2a145e77f1bdec2c4358c7c5
Maria Gabriela <mariagabriela236@gmail.com>
2022-05-16 18:29:56 -0400

    arreglo en regex

--- src/main/java/com/cidenet/cidenet/dto/EmployeeRequest.java
+++ src/main/java/com/cidenet/cidenet/dto/EmployeeRequest.java
@@ -14,18 +14,18 @@ public class EmployeeRequest {
     private StatusEnum status;
 
     @NotEmpty(message = "Primer nombre no puede estar vacio")
-    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Primer nombre invalido")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Primer nombre invalido")
     private String firstName;
 
-    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Segundo nombre invalido")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Segundo nombre invalido")
     private String middleName;
 
     @NotEmpty(message = "Primer apellido no puede estar vacio")
-    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Primer apellido invalido")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Primer apellido invalido")
     private String lastName;
 
     @NotEmpty(message = "Segundo apellido no puede estar vacio")
-    @Pattern(regexp = "^[a-zA-Z]+$", message = "Segundo apellido invalido")
+    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Segundo apellido invalido")
     private String secondLastName;
 
     @NotEmpty(message = "Numero de empleado no puede estar vacio")
